780fac40a8e23c4e97ea3b442031ff697756eea4
Samoylov Aleksey <blinchik232@inbox.ru>
2023-04-02 16:44:23 +0300

    Created main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "Automata.h"
+
+int main() {
+    Automata automata;
+    automata.On();
+    automata.Coin(140);
+    automata.Choice(2);
+    automata.Check();
+    automata.Cook();
+    automata.Finish();
+    automata.Off();
+    return 0;
+}
