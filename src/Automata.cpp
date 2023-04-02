c95baf8fb67808f8c100865cf625c5dffd381e69
Samoylov Aleksey <blinchik232@inbox.ru>
2023-04-02 17:00:09 +0300

    Fix issues

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -23,88 +23,85 @@ static void PrintInfo(STATES state) {
             cout << "Cooking" << endl;
             break;
         default:
-            break;       
+            break;
     }
 }
 
-namespace Automata {
+void Automata::Choice(int pos) {
+    if (position <= MENU_SIZE && position > 0) {
+        if (state == ACCEPT) {
+            state = CHECK;
+            position = pos - 1;
+        }
+    } else {
+        cout << "Invalid value entered" << endl;
+    }
+    PrintInfo(state);
+}
 
-    void Choice(int pos) {
-        if (position <= MENU_SIZE && position > 0) {
-            if (state == ACCEPT) {
-                state = CHECK;
-                position = pos - 1;
-            }
+void Automata::On() {
+    if (state == OFF) {
+        state = WAIT;
+
+        cout << "Menu of Automata" << endl;
+        for (int i = 0; i < MENU_SIZE; i++)  {
+            cout << i + 1 << " position: " << menu[i] << endl;
+        }
+    }
+    PrintInfo(state);
+}
+
+void Automata::Off() {
+    if (state == WAIT) {
+        state = OFF;
+    }
+    PrintInfo(state);
+}
+
+void Automata::Coin(double money) {
+    if (state == WAIT || state == ACCEPT) {
+        state = ACCEPT;
+        cash += money;
+    }
+    PrintInfo(state);
+}
+
+void Automata::Check() {
+    if (state == CHECK) {
+        cout << "---" << cash << endl;
+        if (cash >= prices[position]) {
+            state = COOK;
         } else {
-            cout << "Invalid value entered" << endl;
-        }
-        PrintInfo(state);
-    }
-
-    void On() {
-        if (state == OFF) {
-            state = WAIT;
-
-            cout << "Menu of Automata" << endl;
-            for (int i = 0; i < MENU_SIZE; i++)  {
-                cout << i + 1 << " position: " << menu[i] << endl;
-            }
-        }
-        PrintInfo(state);
-    }
-
-    void Off() {
-        if (state == WAIT) {
-            state = OFF;
-        }
-        PrintInfo(state);
-    }
-
-    void Coin(double money) {
-        if (state == WAIT || state == ACCEPT) {
-            state = ACCEPT;
-            cash += money;
-        }
-        PrintInfo(state);
-    }
-
-    void Check() {
-        if (state == CHECK) {
-            cout << "---" << cash << endl;
-            if (cash >= prices[position]) {
-                state = COOK;
-            } else {
-                cout << "Not enough money" << endl;
-                position = -1;
-                state = WAIT;
-            }
-        }
-        PrintInfo(state);
-    }
-
-    int Cancel() {
-        if (state == ACCEPT || state == CHECK) {
+            cout << "Not enough money" << endl;
             position = -1;
             state = WAIT;
-            double change = cash;
-            cash = 0;
-            PrintInfo(state);
-            return change;
-        }
-        return -1;
-    }
-
-    void Cook() {
-        if (state == COOK)  {
-            cash -= prices[position];
-            cout << "Processing..." << endl;
         }
     }
+    PrintInfo(state);
+}
 
-    void Finish() {
-        if (state == COOK) {
-            state = WAIT;
-        }
+int Automata::Cancel() {
+    if (state == ACCEPT || state == CHECK) {
+        position = -1;
+        state = WAIT;
+        double change = cash;
+        cash = 0;
         PrintInfo(state);
+        return change;
     }
-} // namespace Automata
+    return -1;
+}
+
+void Automata::Cook() {
+    if (state == COOK)  {
+        cash -= prices[position];
+        cout << "Processing..." << endl;
+    }
+}
+
+void Automata::Finish() {
+    if (state == COOK) {
+        state = WAIT;
+    }
+    PrintInfo(state);
+}
